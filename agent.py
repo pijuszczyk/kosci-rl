04cccc02457b68e45f46da5a449800130706511f
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2021-10-05 22:58:23 +0200

    Add a very basic non-ML agent

---
+++ agent.py
@@ -0,0 +1,69 @@
+from abc import abstractmethod
+import numpy as np
+
+import sim
+
+
+class Agent:
+    @abstractmethod
+    def act(self, game: sim.Game):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class _DummyAgent(Agent):
+    def act(self, game: sim.Game):
+        game.accept_roll()
+
+    def __str__(self):
+        return 'Dummy'
+
+
+class _NiegardzacyAgent(Agent):
+    def __init__(self, min_score: int):
+        super().__init__()
+        assert min_score >= 0
+        self.min_score = min_score
+
+    def act(self, game: sim.Game):
+        if game.first_roll_failed:
+            game.accept_roll()
+            return
+        score = sim.calculate_score(game.current_player_kept_dice, game.current_player_new_dice,
+                                    game.current_player_score_in_memory)
+        while score < self.min_score:
+            if sim.can_reroll_every_dice(game.current_player_kept_dice, game.current_player_new_dice):
+                game.do_full_reroll()
+            else:
+                to_keep = [i for i, die in enumerate(game.current_player_new_dice) if die == 1 or die == 5]
+                if len(to_keep) == 0:
+                    # no 1s or 5s, maybe we have a trio or quaddro but whatever #TODO
+                    game.accept_roll()
+                    return
+                if len(to_keep) == len(game.current_player_new_dice):
+                    # something's probably wrong
+                    game.accept_roll()
+                    return
+                game.do_partial_reroll(np.array(to_keep))
+            if game.roll_failed:
+                game.accept_roll()
+                return
+            score = sim.calculate_score(game.current_player_kept_dice, game.current_player_new_dice,
+                                        game.current_player_score_in_memory)
+        game.accept_roll()
+
+    def __str__(self):
+        return f'Niegardzący (min {self.min_score})'
+
+
+class AgentFactory:
+    @staticmethod
+    def make_agent(strategy: str):
+        if strategy == 'dummy':
+            return _DummyAgent()
+        if strategy == 'niegardzący':
+            return _NiegardzacyAgent(40)
+        raise ValueError
