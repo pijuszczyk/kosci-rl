e37192ba6474c94a17995930906d7f03349e1869
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2021-10-06 17:40:19 +0200

    Allow setting seed from main

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ def create_simple_environment(seed: int = 0):
     return env
 
 
-def create_vectorized_environment(num_cpu: int = 8):
+def create_vectorized_environment(n_cpu: int = 8, seed: int = 0):
     def make_env(rank: int, seed: int = 0):
         return create_simple_environment(seed + rank)
-    return sb_vec_env.SubprocVecEnv([lambda: make_env(i) for i in range(num_cpu)])
+    return sb_vec_env.SubprocVecEnv([lambda: make_env(i, seed) for i in range(n_cpu)])
 
 
 def check_env(env):
     sb_env_checker.check_env(env)
 
 
-def train(env, saved_model_path: Optional[str] = None):
-    model = sb.PPO('MultiInputPolicy', env, verbose=1)
+def train(env, saved_model_path: Optional[str] = None, seed: int = 0):
+    model = sb.PPO('MultiInputPolicy', env, seed=seed, verbose=1)
     if saved_model_path is not None:
         model = model.load(saved_model_path, env)
     model.learn(total_timesteps=10000000)
@@ -42,14 +42,17 @@ def test(env, model):
 
 
 def __main__():
-    # env = create_simple_environment()
-    env = create_vectorized_environment()
+    n_cpu = 8
+    seed = 0
+
+    # env = create_simple_environment(seed)
+    env = create_vectorized_environment(n_cpu, seed)
 
     # check_env(env)
 
     path = 'model.zip'
     print('Training')
-    model = train(env, path)
+    model = train(env, path, seed)
     # print('Test')
     # test(env, model)
     print(f'Saving to {path}')
